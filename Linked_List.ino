543c87025243677e851a318c56098665eb5f2740
Dano08 <daniel.gladstone1997@gmail.com>
2018-08-06 14:56:01 -0400

    Small change

--- Linked_List.ino
+++ Linked_List.ino
@@ -1,12 +1,28 @@
 #include "Arduino.h"
 
 typedef struct node{
-    int ID;
+    int id;
     String brand;
+    String colour;
     int quantity;
     struct node* next;    
 };
 
 node* head;
+
+node* createNode(int id, String brand, String colour, int quantity, node* next){
+    
+}
+
+void setup()
+{
+
+}
+
+
+void loop()
+{
+
+}
 
 
